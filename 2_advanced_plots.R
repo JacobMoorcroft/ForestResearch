3a0b4666d199b3d3d00e58590de5d98e7a84ba6f
JacobMoorcroft <jacob.moorcroft2000@gmail.com>
2024-10-02 22:13:19 +0100

    updated html visualisations for interactive graphs

--- 2_advanced_plots.R
+++ 2_advanced_plots.R
@@ -1,14 +1,30 @@
 # While the previous basic plot was initially taken from a Data Science Project completed during my Master's studies, the following code demonstrates a more recent attempt at configuring
 # and advancing upon my previous abilities with data management and visualisation.
 
-## ADDITIONAL PACKAGES:
+## NECESSARY PACKAGES:
+
+library(here)
+library(tidyverse)
+library(cowplot)
+library(magick)
 library(plotly)
+library(htmlwidgets)
 
 # DATA PREPARATION & REORGANISATION
 
 rm(list=setdiff(ls(), c("processed_extracted_data", "woodland_by_year", "BasicPlot"))) # removes unnecessary variables to clean environment
 
-# A new data-frame!
+# mutual code
+
+mapping<-aes(x=year,y=woodland,colour=tree_type) # creates the mapping for the visualisation
+fig_path<-here("figs") # creates the necessary path for saving the figure
+
+
+
+
+
+
+# PUBLIC DATA 
 
 public_trees<-data.frame(
   tree_type=c(rep("FE conifers, England",27),rep("FE broadleaves, England",27),rep("NRW conifers, Wales",27),rep("NRW broadleaves, Wales",27),
@@ -21,18 +37,17 @@ public_trees<-data.frame(
 
 public_trees$year<-as.numeric(public_trees$year) # ensures is numeric so it can be amenable to analysis*
 public_trees$woodland<-as.numeric(public_trees$woodland) # *
+public_trees$tree_type<-factor(public_trees$tree_type, levels=c("FE conifers, England", "FE broadleaves, England", 
+                                                                  "NRW conifers, Wales", "NRW broadleaves, Wales",
+                                                                  "FLS conifers, Scotland", "FLS broadleaves, Scotland",
+                                                                  "FS conifers, NI", "FS broadleaves, NI"))
+## BASIC INTERACTIVE PLOT:
 
-mapping<-aes(x=year,y=woodland,colour=tree_type) # creates the mapping for the visualisation
-fig_path<-here("figs") # creates the necessary path for saving the figure
-
-## ADVANCED PLOT:
-
-InteractivePlot<-public_trees %>%
+InteractivePlot_public<-public_trees %>%
   ggplot(mapping=mapping)+
   geom_point(aes(text = paste("Woodland Area:", woodland, "(K ha)",
                                "<br>Year:", year, "<br>Type & Region:", tree_type), shape=tree_type))+
-  geom_smooth(aes(text=NULL),method=lm, se=NULL)+
-  labs(title="Graph of Public Conifer and Broadleaf Woodland Area Within the United Kingdom",
+  labs(title="The Growth of Coniferous and Deciduous (Broadleaf) Public Woodland Area in the United Kingdom",
        x="Year (commencing from March 31st)",
        y="Woodland area (in thousand hectares)",
        colour="Woodland Type and Region",
@@ -43,19 +58,98 @@ InteractivePlot<-public_trees %>%
                                `FS conifers, NI`="#FFA900",`FS broadleaves, NI`="#FFA900"))+
   scale_shape_manual(values=c(0,15,1,16,2,17,5,18))+
   scale_x_continuous(breaks=seq(1998,2024,4))+
-  scale_y_continuous(breaks=seq(0,500,50))+
+  scale_y_continuous(breaks=seq(0,1000,50))+
   theme(panel.border=element_rect(colour="darkgreen",fill=NA,linewidth=2),
-        axis.line=element_line(linewidth=2,colour="black"),
+        panel.grid.major=element_line(colour="lightgreen",linewidth=0.7),
+        axis.line=element_line(linewidth=2,colour="darkgreen"),
         panel.background=element_rect(fill="white"),
         plot.title=element_text(face="bold"),
         text=element_text(family="sans"))
 
-# NOTE - caption does not currently work, nor does the brown graph lines (border does)
+# Unfortunately, the logo cannot be added to an interactive plot as this feature has not yet been implemented into plotly
 
-InteractivePlot<-ggplotly(InteractivePlot, tooltip="text") # creates extremely basic interactive graph
+# makes interactive
 
-InteractivePlot
+InteractivePlot_public<-ggplotly(InteractivePlot_public, tooltip="text",width=900,height=600) %>%
+  layout(
+    margin = list(t = 50, r = 50, b = 50, l = 50)
+    )
 
-# NOTE - play around with further ways to modify the interactive graph, i.e. moving up in yearly increments of 1 > decimals, therefore also jumping woodland amounts to be per year
+InteractivePlot_public
 
-rm(list=setdiff(ls(), c("processed_extracted_data", "woodland_by_year", "public_trees", "BasicPlot", "InteractivePlot"))) # removes unnecessary variables to clean environment
+# saves
+
+filename<-paste("The Growth of Coniferous & Deciduous (Broadleaf) Public Woodland Area in the UK from 1998 to 2024.html",sep="")
+full_file_path<-file.path(fig_path,filename)
+saveWidget(InteractivePlot_public, file=full_file_path)
+
+
+
+
+
+
+
+
+# PRIVATE DATA
+
+private_trees<-data.frame(
+  tree_type=c(rep("Private sector conifers, England",27),rep("Private sector broadleaves, England",27),rep("Private sector conifers, Wales",27),rep("Private sector broadleaves, Wales",27),
+              rep("Private sector conifers, Scotland",27),rep("Private sector broadleaves, Scotland",27),rep("Private sector conifers, NI",27),rep("Private sector broadleaves, NI",27)),
+  woodland=c(processed_extracted_data$`Private sector conifers (thousand ha)`,processed_extracted_data$`Private sector broadleaves (thousand ha)`,
+             processed_extracted_data$`Private sector conifers (thousand ha).1`,processed_extracted_data$`Private sector broadleaves (thousand ha).1`,
+             processed_extracted_data$`Private sector conifers (thousand ha).2`,processed_extracted_data$`Private sector broadleaves (thousand ha).2`,
+             processed_extracted_data$`Private sector conifers (thousand ha).3`,processed_extracted_data$`Private sector broadleaves (thousand ha).3`),
+  year=c(woodland_by_year$year_ending_March_31st)) # creates a dataframe amenable to the upcoming visualisation
+
+private_trees$year<-as.numeric(private_trees$year) # ensures is numeric so it can be amenable to analysis*
+private_trees$woodland<-as.numeric(private_trees$woodland) # *
+private_trees$tree_type<-factor(private_trees$tree_type, levels=c("Private sector conifers, England", "Private sector broadleaves, England", 
+                                                                  "Private sector conifers, Wales", "Private sector broadleaves, Wales",
+                                                                  "Private sector conifers, Scotland", "Private sector broadleaves, Scotland",
+                                                                  "Private sector conifers, NI", "Private sector broadleaves, NI"))
+
+## BASIC INTERACTIVE PLOT:
+
+InteractivePlot_private<-private_trees %>%
+  ggplot(mapping=mapping)+
+  geom_point(aes(text = paste("Woodland Area:", woodland, "(K ha)",
+                              "<br>Year:", year, "<br>Type & Region:", tree_type), shape=tree_type))+
+  labs(title="The Growth of Coniferous and Deciduous (Broadleaf) Private Woodland Area in the United Kingdom",
+       x="Year (commencing from March 31st)",
+       y="Woodland area (in thousand hectares)",
+       colour="Woodland Type and Region",
+       shape=NULL)+
+  scale_colour_manual(values=c(`Private sector conifers, England`="#FF0000",`Private sector broadleaves, England`="#FF0000",
+                               `Private sector conifers, Wales`="#00CD00",`Private sector broadleaves, Wales`="#00CD00",
+                               `Private sector conifers, Scotland`="#0000CD",`Private sector broadleaves, Scotland`="#0000CD",
+                               `Private sector conifers, NI`="#FFA900",`Private sector broadleaves, NI`="#FFA900"))+
+  scale_shape_manual(values=c(0,15,1,16,2,17,5,18))+
+  scale_x_continuous(breaks=seq(1998,2024,4))+
+  scale_y_continuous(breaks=seq(0,1000,100))+
+  theme(panel.border=element_rect(colour="darkgreen",fill=NA,linewidth=2),
+        panel.grid.major=element_line(colour="lightgreen",linewidth=0.7),
+        axis.line=element_line(linewidth=2,colour="darkgreen"),
+        panel.background=element_rect(fill="white"),
+        plot.title=element_text(face="bold"),
+        text=element_text(family="sans"))
+
+# makes interactive
+
+InteractivePlot_private<-ggplotly(InteractivePlot_private, tooltip="text",width=900,height=600) %>%
+  layout(
+    margin = list(t = 50, r = 50, b = 50, l = 50)
+  )
+
+InteractivePlot_private
+
+# saves
+
+fig_path<-here("figs") # creates the necessary path for saving the figure
+filename<-paste("The Growth of Coniferous & Deciduous (Broadleaf) Private Woodland Area in the UK from 1998 to 2024.html",sep="")
+full_file_path<-file.path(fig_path,filename)
+saveWidget(InteractivePlot_private, file=full_file_path)
+
+
+
+
+# you need to make both graph plots on the same html scale so that they can be portrayed side by side and not be misleading :)
